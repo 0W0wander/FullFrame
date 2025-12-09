1df0e4c052655e17f6013e52a407f5552e5cc8ea
FullFrame Developer <dev@fullframe.app>
2025-12-09 10:10:52 -0500

    Add CMake build system and basic main window

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <QApplication>
+#include <QMainWindow>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    app.setApplicationName("FullFrame");
+    
+    QMainWindow window;
+    window.setWindowTitle("FullFrame");
+    window.setMinimumSize(800, 600);
+    window.show();
+    
+    return app.exec();
+}

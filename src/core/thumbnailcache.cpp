12a7f27fae101686edd2056a6660e4163a3d41ec
FullFrame Developer <dev@fullframe.app>
2025-12-11 09:14:00 -0500

    Add ThumbnailCache with LRU eviction

---
+++ src/core/thumbnailcache.cpp
@@ -0,0 +1,73 @@
+﻿#include "thumbnailcache.h"
+
+namespace FullFrame {
+
+ThumbnailCache* ThumbnailCache::s_instance = nullptr;
+
+ThumbnailCache* ThumbnailCache::instance()
+{
+    if (!s_instance) {
+        s_instance = new ThumbnailCache();
+    }
+    return s_instance;
+}
+
+void ThumbnailCache::cleanup()
+{
+    delete s_instance;
+    s_instance = nullptr;
+}
+
+ThumbnailCache::ThumbnailCache(QObject* parent)
+    : QObject(parent)
+{
+}
+
+ThumbnailCache::~ThumbnailCache()
+{
+    clearAll();
+}
+
+const QImage* ThumbnailCache::retrieveImage(const QString& cacheKey) const
+{
+    QReadLocker locker(&m_imageLock);
+    auto it = m_imageCache.find(cacheKey);
+    if (it != m_imageCache.end()) {
+        return &it.value();
+    }
+    return nullptr;
+}
+
+void ThumbnailCache::putImage(const QString& cacheKey, const QImage& image)
+{
+    QWriteLocker locker(&m_imageLock);
+    m_imageCache.insert(cacheKey, image);
+    m_imageLRU.prepend(cacheKey);
+    
+    while (m_imageCache.size() > m_maxImages && !m_imageLRU.isEmpty()) {
+        QString key = m_imageLRU.takeLast();
+        m_imageCache.remove(key);
+    }
+}
+
+bool ThumbnailCache::hasImage(const QString& cacheKey) const
+{
+    QReadLocker locker(&m_imageLock);
+    return m_imageCache.contains(cacheKey);
+}
+
+void ThumbnailCache::clearAll()
+{
+    QWriteLocker locker(&m_imageLock);
+    m_imageCache.clear();
+    m_imageLRU.clear();
+    Q_EMIT cacheCleared();
+}
+
+int ThumbnailCache::imageCacheCount() const
+{
+    QReadLocker locker(&m_imageLock);
+    return m_imageCache.size();
+}
+
+} // namespace FullFrame
---
+++ src/core/thumbnailcache.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include <QObject>
+#include <QImage>
+#include <QPixmap>
+#include <QHash>
+#include <QMutex>
+#include <QReadWriteLock>
+
+namespace FullFrame {
+
+class ThumbnailCache : public QObject
+{
+    Q_OBJECT
+
+public:
+    static ThumbnailCache* instance();
+    static void cleanup();
+
+    const QImage* retrieveImage(const QString& cacheKey) const;
+    void putImage(const QString& cacheKey, const QImage& image);
+    bool hasImage(const QString& cacheKey) const;
+    
+    void clearAll();
+    int imageCacheCount() const;
+
+Q_SIGNALS:
+    void cacheCleared();
+
+private:
+    explicit ThumbnailCache(QObject* parent = nullptr);
+    ~ThumbnailCache() override;
+
+    static ThumbnailCache* s_instance;
+    
+    mutable QReadWriteLock m_imageLock;
+    QHash<QString, QImage> m_imageCache;
+    QList<QString> m_imageLRU;
+    int m_maxImages = 500;
+};
+
+} // namespace FullFrame

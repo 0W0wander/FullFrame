fabbf4f270f0a7edc970a76cf30372068328c5aa
FullFrame Developer <dev@fullframe.app>
2025-12-16 22:00:58 -0500

    Add ImageThumbnailModel for grid data

---
+++ src/models/imagethumbnailmodel.cpp
@@ -0,0 +1,109 @@
+﻿#include "imagethumbnailmodel.h"
+#include "thumbnailloadthread.h"
+#include "thumbnailcreator.h"
+#include <QDir>
+#include <QFileInfo>
+
+namespace FullFrame {
+
+ImageThumbnailModel::ImageThumbnailModel(QObject* parent)
+    : QAbstractListModel(parent)
+{
+    connect(ThumbnailLoadThread::instance(), &ThumbnailLoadThread::thumbnailReady,
+            this, &ImageThumbnailModel::onThumbnailReady);
+}
+
+ImageThumbnailModel::~ImageThumbnailModel() = default;
+
+int ImageThumbnailModel::rowCount(const QModelIndex& parent) const
+{
+    if (parent.isValid()) return 0;
+    return m_files.count();
+}
+
+QVariant ImageThumbnailModel::data(const QModelIndex& index, int role) const
+{
+    if (!index.isValid() || index.row() >= m_files.count())
+        return QVariant();
+
+    const QString& path = m_files.at(index.row());
+
+    switch (role) {
+    case Qt::DisplayRole:
+    case FileNameRole:
+        return QFileInfo(path).fileName();
+    case FilePathRole:
+        return path;
+    case Qt::DecorationRole:
+    case ThumbnailRole:
+        if (m_thumbnails.contains(path)) {
+            return m_thumbnails.value(path);
+        }
+        ThumbnailLoadThread::instance()->load(path, m_thumbnailSize);
+        return QVariant();
+    default:
+        return QVariant();
+    }
+}
+
+void ImageThumbnailModel::setDirectory(const QString& path)
+{
+    beginResetModel();
+    clear();
+    scanDirectory(path);
+    endResetModel();
+}
+
+void ImageThumbnailModel::clear()
+{
+    m_files.clear();
+    m_thumbnails.clear();
+    m_pathToIndex.clear();
+}
+
+QString ImageThumbnailModel::filePath(int index) const
+{
+    if (index >= 0 && index < m_files.count()) {
+        return m_files.at(index);
+    }
+    return QString();
+}
+
+int ImageThumbnailModel::indexOf(const QString& path) const
+{
+    return m_pathToIndex.value(path, -1);
+}
+
+void ImageThumbnailModel::onThumbnailReady(const QString& filePath, const QPixmap& pixmap)
+{
+    int idx = indexOf(filePath);
+    if (idx >= 0) {
+        m_thumbnails.insert(filePath, pixmap);
+        QModelIndex modelIdx = index(idx);
+        Q_EMIT dataChanged(modelIdx, modelIdx, {Qt::DecorationRole, ThumbnailRole});
+    }
+}
+
+void ImageThumbnailModel::scanDirectory(const QString& path)
+{
+    QDir dir(path);
+    QStringList filters;
+    for (const QString& ext : ThumbnailCreator::imageExtensions()) {
+        filters << "*." + ext;
+    }
+    
+    QFileInfoList files = dir.entryInfoList(filters, QDir::Files, QDir::Name);
+    
+    Q_EMIT loadingStarted(files.count());
+    
+    int index = 0;
+    for (const QFileInfo& info : files) {
+        m_files.append(info.absoluteFilePath());
+        m_pathToIndex.insert(info.absoluteFilePath(), index++);
+        Q_EMIT loadingProgress(index, files.count());
+    }
+    
+    Q_EMIT loadingFinished();
+}
+
+} // namespace FullFrame
---
+++ src/models/imagethumbnailmodel.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+#include <QAbstractListModel>
+#include <QStringList>
+#include <QPixmap>
+#include <QHash>
+
+namespace FullFrame {
+
+class ImageThumbnailModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    enum Roles {
+        FilePathRole = Qt::UserRole + 1,
+        FileNameRole,
+        ThumbnailRole
+    };
+
+    explicit ImageThumbnailModel(QObject* parent = nullptr);
+    ~ImageThumbnailModel() override;
+
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+
+    void setDirectory(const QString& path);
+    void clear();
+    
+    QString filePath(int index) const;
+    int indexOf(const QString& path) const;
+
+Q_SIGNALS:
+    void loadingStarted(int total);
+    void loadingProgress(int current, int total);
+    void loadingFinished();
+
+private Q_SLOTS:
+    void onThumbnailReady(const QString& filePath, const QPixmap& pixmap);
+
+private:
+    void scanDirectory(const QString& path);
+
+    QStringList m_files;
+    QHash<QString, QPixmap> m_thumbnails;
+    QHash<QString, int> m_pathToIndex;
+    int m_thumbnailSize = 256;
+};
+
+} // namespace FullFrame
